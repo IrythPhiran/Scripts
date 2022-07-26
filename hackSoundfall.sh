f2ea1f039fb95f8aeb1ee908defd0544b79cea02
IrythPhiran <59703283+IrythPhiran@users.noreply.github.com>
2022-07-26 17:25:31 +0100

    Update hackSoundfall.sh

--- hackSoundfall.sh
+++ hackSoundfall.sh
@@ -22,3 +22,10 @@ cp ES_Cara\ a\ Cara\ -\ Hugo\ Correia.ogg $HOME/.var/app/com.valvesoftware.Steam
 echo "Replacing ingame .bsaa File"
 cp "$2" $HOME/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/SoundfallGame/BeatShooter/Content/AudioAnalysisCache/ES_Cara\ a\ Cara\ -\ Hugo\ Correia.bsaa
 cp "$2" $HOME/Documents/SoundFall
+
+# This script converts a custom music file ($1) into the .ogg file format (as it tends to yield better results when analysed by the game's AI) 
+# and then renames & replaces the ingame file "ES_Cara a Cara - Hugo Correia.ogg" with the custom song ($1) as well as the BSAA analysis file ($2)
+# generated for the map analysis of the song.
+#
+# This hack/mod effectively allows us to bypass the lock on Custom Songs when playing Online with other players
+# as long as all players in the lobby share the same custom converted song ($1) and analysis for the map ($2) 
